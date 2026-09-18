cc623182b61b3064d6a20450b46a24ff4fc987f5
GodTek <nini.cambindo@correounivalle.edu.co>
2026-09-17 21:58:30 -0500

    chore: configurar Next.js y Turbopack (#5)
    Co-authored-by: ninijh <nini08ca@gmail.com>

--- proyectos/frontend/next.config.ts
+++ proyectos/frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
